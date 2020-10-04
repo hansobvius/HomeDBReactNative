e53c2857933e23c6ea12a1f4371b891c6bbb7ca5
Thiago Caiado <thiago.caiado@hotmail.com>
2020-10-04 15:43:39 -0300

    impl service api request

--- App/Screens/Home.tsx
+++ App/Screens/Home.tsx
@@ -1,10 +1,30 @@
 import React from  'react'
-import { FlatList } from 'react-native'
+import { StyleSheet, FlatList } from 'react-native'
+import * as service from '../ServiceApi/service'
 
 const Home = () => {
+    const movies = service.serviceRequest()
     return(
-        <></>
+        <>
+            <FlatList
+                data={[movies]}
+                renderItem={()=> <></>}
+            />
+        </>
     )
 }
+
+const rowList = () => {
+    return(
+        <>
+            <FlatList
+                data={[]}
+                renderItem={()=><></>}
+            />
+        </>
+    )
+}
+
+const style = StyleSheet.create({})
 
 export default Home
---
+++ App/ServiceApi/service.tsx
@@ -0,0 +1,20 @@
+export const TOKEN = '38eeef9aa65a725363ccb5cde9df6342'
+
+export const BASE_URL = 'https://api.themoviedb.org/3/movie/'
+
+export const DEFAULT_LANGUAGE = 'en-us'
+
+export const DEFAULT_PAGE = 1
+
+export const POPULAR_ENDPOINT = 'popular'
+
+export const serviceRequest = () =>{
+    return fetch(`${BASE_URL}${POPULAR_ENDPOINT}?api_key=${TOKEN}&language=${DEFAULT_LANGUAGE}&page=${DEFAULT_PAGE}`)
+        .then((response) => response.json)
+        .then((json) => {
+            return json
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
