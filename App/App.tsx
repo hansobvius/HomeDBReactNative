d82941f540e352154088834d0e953e97669805ba
ThiagoCaiado <cin_tcaiado@uolinc.com>
2020-10-25 18:59:41 -0300

    impl navigation stack

--- App/App.tsx
+++ App/App.tsx
@@ -1,12 +1,28 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 import Home from './Screens/home/Home'
+import Detail from './Screens/detail/Detail'
+
+const stack = createStackNavigator()
 
 const App = () => {
   return (
     <> 
       <NavigationContainer>
-        <Home/> 
+        <stack.Navigator initialRouteName="Home">
+          <stack.Screen
+            name="Home"
+            component={Home}
+            options={{ title: "MovieDB" }}
+          />
+          <stack.Screen
+            name="Detail"
+            component={Detail}
+            options={{ title: "MovieDB" }}
+          />
+        </stack.Navigator>
       </NavigationContainer>
     </>
   );
